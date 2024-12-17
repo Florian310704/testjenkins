88201053dee1fef12c08e42e0daa8ec6e0b447d1
Florian310704 <connartflorian59@gmail.com>
2024-12-17 16:21:42 +0100

    hhhh

--- src/test/java/com/example/todolist/TodoControllerTest.java
+++ src/test/java/com/example/todolist/TodoControllerTest.java
@@ -3,6 +3,7 @@ package com.example.todolist;
 import com.example.todolist.controller.TodoController;
 import com.example.todolist.model.ToDo;
 import com.example.todolist.service.TodoService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -25,25 +26,23 @@ public class TodoControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    // Remplacement de @MockBean par @Mock
     @Mock
     private TodoService todoService;
 
     @InjectMocks
     private TodoController todoController;
 
-    public TodoControllerTest() {
-        MockitoAnnotations.openMocks(this); // initialise les mocks
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
     public void testGetAllTodos() throws Exception {
-        // Mock du service
         ToDo todo1 = new ToDo(1L, "Task 1", false);
         ToDo todo2 = new ToDo(2L, "Task 2", true);
         when(todoService.getAllTodos()).thenReturn(Arrays.asList(todo1, todo2));
 
-        // Appel et vérification
         mockMvc.perform(get("/todos")
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
